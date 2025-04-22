a5ce3a3e8df6a4dc0408a72a6c80d0c575ed8d09
NorOmar <onoor2143@gmail.com>
2025-04-22 14:45:10 +0200

    up

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ console.log(input);
 //https://api.weatherapi.com/v1/search.json?key=e5e884ed3d074c9580984541252104&q=${key}
 
 async function fetchData(key) {
-    var response= await fetch(`http://api.weatherapi.com/v1/forecast.json?key=e5e884ed3d074c9580984541252104&q=${key}&days=3`);
+    var response= await fetch(`https://api.weatherapi.com/v1/forecast.json?key=e5e884ed3d074c9580984541252104&q=${key}&days=3`);
     data = await response.json();
     console.log(data);
     displayData(data);  
